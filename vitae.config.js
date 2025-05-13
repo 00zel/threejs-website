661c05adbc5947d6d7f95856549d38d13c944d28
00zel <elizabethgracedowney@gmail.com>
2025-05-13 12:32:19 -0400

    Clean reset: remove GitHub Pages setup

--- vitae.config.js
+++ vitae.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    server: {
-        mimeTypes: {
-            'model/gltf-binary': ['glb']
-        }
+  server: {
+    mimeTypes: {
+      'model/gltf-binary': ['glb'], // Ensuring GLB files are recognized
     }
+  },
+  build: {
+    outDir: 'dist', // Output folder for the production build
+    rollupOptions: {
+      input: 'index.html', // Make sure index.html is used as the entry point
+      
+    }
+  }
 });
